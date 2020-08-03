24ba70e35e072649e72b5d793ee410c00192b7bc
JohnNils Olson <johnnils@gmail.com>
2020-08-03 11:03:42 -0700

    update for many to many database structure
    Co-authored-by: Thomas Glenn <thomasgrantglenn@mac.com>

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,26 +1,25 @@
-// using Microsoft.AspNetCore.Mvc;
-// using ToDoList.Models;
-// using System.Collections.Generic;
-// using System.Linq;
-// using Microsoft.EntityFrameworkCore;
-// using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
-// namespace ToDoList.Controllers
-// {
-//   public class ItemsController : Controller
-//   {
-//     private readonly ToDoListContext _db;
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    private readonly ToDoListContext _db;
 
-//     public ItemsController(ToDoListContext db)
-//     {
-//       _db = db;
-//     }
+    public ItemsController(ToDoListContext db)
+    {
+      _db = db;
+    }
 
-//     public ActionResult Index()
-//     {
-//       List<Item> model = _db.Items.Include(items => items.Category).ToList();
-//       return View(model);
-//     }
+    public ActionResult Index()
+    {
+      return View(_db.Items.ToList());
+    }
 
 //     public ActionResult Create()
 //     {
@@ -36,11 +35,14 @@
 //       return RedirectToAction("Index");
 //     }
 
-//     public ActionResult Details(int id)
-//     {
-//       Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-//       return View(thisItem);
-//     }
+      public ActionResult Details(int id)
+      {
+        var thisItem = _db.Items 
+            .Include(item => item.Categories)
+            .ThenInclude(join => join.Category)
+            .FirstOrDefault(item => item.ItemId == id);
+        return View(thisItem);
+      }
 
 //     public ActionResult Edit(int id)
 //     {
@@ -71,5 +73,5 @@
 //       _db.SaveChanges();
 //       return RedirectToAction("Index");
 //     }
-//   }
-// }
+  }
+}
